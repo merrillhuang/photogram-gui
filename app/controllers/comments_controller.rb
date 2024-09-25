7182fe456333b66533c28034429092e066f70145
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-25 20:55:57 +0000

    remove unnecessary local variable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,8 @@
 class CommentsController < ApplicationController
   def create
-
-    photo_id = params.fetch("query_photo_id")
-
     new_comment = Comment.new(
       {
-        photo_id: photo_id,
+        photo_id: params.fetch("query_photo_id"),
         author_id: params.fetch("query_author_id"),
         body: params.fetch("query_comment_body")
       }
@@ -13,9 +10,9 @@ class CommentsController < ApplicationController
 
     if new_comment.valid?
       new_comment.save
-      redirect_to("/photos/#{photo_id}", notice: "Comment created successfully")
+      redirect_to("/photos/#{new_comment.photo_id}", notice: "Comment created successfully")
     else
-      redirect_to("/photos/#{photo_id}", notice: "Comment failed to create successfully")
+      redirect_to("/photos/#{new_comment.photo_id}", notice: "Comment failed to create successfully")
     end
   end
 end
