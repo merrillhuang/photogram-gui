b4dcdda38f268d8903653e3016ced5206bd59ff3
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-25 20:51:35 +0000

    define create method for comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,21 @@
 class CommentsController < ApplicationController
-  
+  def create
+
+    photo_id = params.fetch("query_photo_id")
+
+    new_comment = Comment.new(
+      {
+        photo_id: photo_id,
+        author_id: params.fetch("query_author_id"),
+        body: params.fetch("query_comment_body")
+      }
+    )
+
+    if new_comment.valid?
+      new_comment.save
+      redirect_to("/photos/#{photo_id}", notice: "Comment created successfully")
+    else
+      redirect_to("/photos/#{photo_id}", notice: "Comment failed to create successfully")
+    end
+  end
 end
