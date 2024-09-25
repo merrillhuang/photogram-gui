e6f5831a80b9fb0836758f4eef43ac977b6f106f
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-25 18:03:44 +0000

    create insert_user route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get("/", {controller: "users", action: "index"})
 
   get("/users", {controller: "users", action: "index"})
+  post("/insert_user", {controller: "users", action: "create"})
 end
