29d833809075d2c5868901bde9c44d79b3f80854
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-25 18:10:50 +0000

    define create method for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
 
     render("/user_templates/index")
   end
+  
+  def create
+    new_user = User.new({username: params.fetch("query_username")})
+
+    if new_user.valid?
+      new_user.save
+      redirect_to("/users", {notice: "User created successfuly"})
+    else
+      redirect_to("/users", {notice: "User failed to create successfully"})
+    end
+  end
 end
