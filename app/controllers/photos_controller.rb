4fd84f240483feab020fbbf5012c4e282668289d
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-25 20:00:59 +0000

    define create method for photos_controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,21 @@ class PhotosController < ApplicationController
 
     render("photo_templates/index")
   end
+
+  def create
+    new_photo = Photo.new(
+      {
+        image: params.fetch("query_image_url"),
+        caption: params.fetch("query_caption"),
+        owner_id: params.fetch("query_owner_id")
+      }
+    )
+
+    if new_photo.valid?
+      new_photo.save
+      redirect_to("/photos", {notice: "Photo created successfully"})
+    else
+      redirect_to("/photos", {notice: "Photo failed to create successfully"})
+    end
+  end
 end
